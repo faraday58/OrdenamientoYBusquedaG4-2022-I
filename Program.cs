85a78b2ac0b79d096570052640aa1bbfac50aa3b
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-11-17 13:59:50 -0600

    Algoritmo de Arreglo aleatorio

--- Program.cs
+++ Program.cs
@@ -6,6 +6,35 @@ namespace OrdenamientoYBusquedaG4_2022_I
     {
         static void Main()
         {
+            int[] A = { 5, 3, 9, 7, 10, 1 };
+            ImprimeArreglo(A);
+            Ordenamiento.Burbuja(A);
+            ImprimeArreglo(A);
+            Console.ReadLine();
+
+        }
+
+
+        public int [] ArregloAleatorio(int N)
+        {
+            int[] A = new int[N];
+            Random random = new Random();
+            for(int i=0; i< N; i++)
+            {
+                A[i] = (int)(random.NextDouble()*1000);
+            }
+            return A;
+
+        }
+
+
+        public static void ImprimeArreglo(int [] A)
+        {
+            foreach(int elemento in A)
+            {
+                Console.Write("{0},", elemento);
+            }
+            Console.WriteLine();
         }
     }
 }
