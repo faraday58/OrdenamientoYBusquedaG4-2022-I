87b1bee2c652a377182369d0cea7a0d486a13e47
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-11-17 14:20:23 -0600

    Busqueda

---
+++ Busqueda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OrdenamientoYBusquedaG4_2022_I
+{
+    class Busqueda
+    {
+        public int Directa(int[] A, int X )
+        {
+            int N = A.Length;
+            for(int i=0; i< N; i++)
+            {
+                if(  A[i]  == X   )
+                {
+                    return i;
+                }                
+            }
+
+            return -1;
+
+        }
+
+    }
+}
